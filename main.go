f9ee893c7b88e9673b259c44e3be691021afc398
geraud simon <geraud.simon99@gmail.com>
2021-10-17 17:06:42 +0200

    creation branche

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+/*import (
+    "log"
+    "net/http"
+
+    //"github.com/Rouret/golangProject/api"
+)*/
+
+func main() {
+    /*err := http.ListenAndServe(":3000", api.Handlers())
+
+    if err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }*/
+}
