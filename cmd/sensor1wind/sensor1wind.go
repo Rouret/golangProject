64fc97c241b00c5c7cc3d3909a4d13ccc5f3fa68
Lucas ROURET <lucas.rouretperso@gmail.com>
2021-10-30 20:34:21 +0200

    add wind and pressure (#5)

---
+++ cmd/sensor1wind/sensor1wind.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"strconv"
+	"time"
+
+	"github.com/Rouret/golangProject/internal/config"
+	"github.com/Rouret/golangProject/internal/random"
+	"github.com/Rouret/golangProject/internal/sensor"
+	"github.com/Rouret/mqtt.golang"
+)
+
+func main() {
+	config := config.GetConfig()
+	brokerPort := strconv.Itoa(config.BrokerPort)
+	idClient := strconv.Itoa(config.ID)
+	
+	mqtt.Setup(mqtt.LibConfiguration{
+		IsPersistent: true,
+	})
+
+	mqtt.Connect(config.BrokerUrl+":"+brokerPort, idClient)
+	
+	for range time.Tick(time.Second * time.Duration(config.DelayMessage)) {
+		message := sensor.Message{
+			IdCapteur: config.ID,
+			IATA:      config.IATA,
+			TypeValue: config.ValueType,
+			Value:     random.GetRandomFloat(10, 50),
+			Timestamp: time.Now().Unix(),
+		}
+		mqtt.Send(config.Topic,config.QOS,message,true)
+	}
+}
